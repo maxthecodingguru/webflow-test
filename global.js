953eca48340302d55e30e3548e06d54b89cf5056
maxthecodingguru <163391846+maxthecodingguru@users.noreply.github.com>
2024-03-14 08:38:39 +0100

    Add files via upload

---
+++ global.js
@@ -0,0 +1,1 @@
+!function(o,e){"object"==typeof exports&&"object"==typeof module?module.exports=e():"function"==typeof define&&define.amd?define("global",[],e):"object"==typeof exports?exports.global=e():o.global=e()}(this,(()=>(gsap.fromTo("h1",{y:25,autoAlpha:0},{y:0,autoAlpha:1}),alert("github"),{})));
